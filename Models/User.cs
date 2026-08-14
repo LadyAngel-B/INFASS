783ae71cc203c69a3a9ab83fbefdf26b47b83a6a
ACT-STUDENT <ACT-STUDENT@LAB5-PC5>
2026-08-14 16:55:57 +0800

    A

--- Models/User.cs
+++ Models/User.cs
@@ -2,22 +2,28 @@
 {
     public class User
     {
-        public int Id { get; set; }
-        public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
 
-        public User(string fullName, string email, string password)
+        public User(
+            string fullName,
+            string email,
+            string password)
         {
             FullName = fullName;
             Email = email;
             Password = password;
         }
 
-
-        public string GenerateInsertQuery(string tableName, string[] fields, object[] values)
+        public string GenerateInsertQuery(
+            string tableName,
+            string[] fields,
+            object[] values)
         {
-            string query = "INSERT INTO " + tableName + " (";
+            string query =
+                "INSERT INTO " + tableName + " (";
 
             for (int i = 0; i < fields.Length; i++)
             {
@@ -46,5 +52,3 @@
         }
     }
 }
-
-
