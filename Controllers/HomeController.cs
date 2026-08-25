37f49a051173d3867d21e1f9fba96d6f9440a459
ACT-STUDENT <ACT-STUDENT@LAB2-PC20>
2026-08-25 14:26:31 +0800

    database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using INFASS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 
@@ -7,14 +8,14 @@ namespace INFASS.Controllers
 {
     public class HomeController : Controller
     {
-             private readonly ILogger<HomeController> _logger;
-              private readonly string _connectionString;
+        private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString;
 
-     public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
-     {
-         _logger = logger;
-         _connectionString = configuration.GetConnectionString("DefaultConnection");
-     }
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
         public IActionResult Index()
         {
             return View();
@@ -37,6 +38,18 @@ namespace INFASS.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public IActionResult Register([FromBody] RegisterViewModel model)
+        //{
+        //    User user = new User(model.FullName, model.Email, model.Password);
+
+        //    string[] fields = { "Name", "Email", "Password" };
+        //    object[] values = { model.FullName, model.Email, model.Password };
+
+        //    string query = user.GenerateInsertQuery("Users", fields, values);
+
+        //    return Json(new { query = query });
+        //}
 
 
         [HttpPost]
@@ -48,6 +61,105 @@ namespace INFASS.Controllers
             object[] values = { model.FullName, model.Email, model.Password };
 
             string query = user.GenerateInsertQuery("Users", fields, values);
+
+            string connectionString = "Data Source=LAB2-PC20\\LAB3PC44;Initial Catalog=INFASSDB;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return Json(new { query = query });
+            //return Json(new { message = "User registered successfully" });
+        }
+
+        // SELECT
+        [HttpPost]
+        public IActionResult SelectUser([FromBody] LoginViewModel model)
+        {
+            User user = new User(
+                "",
+                model.Email,
+                ""
+            );
+
+            string query = user.GenerateSelectQuery(
+                "Users",
+                "Email",
+                model.Email
+            );
+
+            return Json(new { query = query });
+        }
+
+        // UPDATE
+        [HttpPost]
+        public IActionResult Update([FromBody] RegisterViewModel model)
+        {
+            User user = new User(
+                model.FullName,
+                model.Email,
+                model.Password
+            );
+
+            string[] fields = { "Name", "Password" };
+
+            object[] values =
+            {
+        model.FullName,
+        model.Password
+    };
+
+            string query = user.GenerateUpdateQuery(
+                "Users",
+                fields,
+                values,
+                "Email",
+                model.Email
+            );
+
+            return Json(new { query = query });
+        }
+
+
+        // DELETE
+        [HttpPost]
+        public IActionResult Delete([FromBody] LoginViewModel model)
+        {
+            User user = new User(
+                "",
+                model.Email,
+                ""
+            );
+
+            string query = user.GenerateDeleteQuery(
+                "Users",
+                "Email",
+                model.Email
+            );
+
+            return Json(new { query = query });
+        }
+
+        // VIEW PAGE
+        public IActionResult ViewUser()
+        {
+            return View();
+        }
+
+        // SELECT
+        [HttpPost]
+        public IActionResult SelectUser([FromBody] LoginViewModel model)
+        {
+            User user = new User("", model.Email, "");
+
+            string query = user.GenerateSelectQuery(
+                "Users",
+                "Email",
+                model.Email
+            );
 
             return Json(new { query = query });
         }
--- Models/User.cs
+++ Models/User.cs
@@ -14,8 +14,11 @@
             Password = password;
         }
 
-
-        public string GenerateInsertQuery(string tableName, string[] fields, object[] values)
+        // CREATE / INSERT
+        public string GenerateInsertQuery(
+            string tableName,
+            string[] fields,
+            object[] values)
         {
             string query = "INSERT INTO " + tableName + " (";
 
@@ -45,6 +48,75 @@
             return query;
         }
 
+        // SELECT
+        public string GenerateSelectQuery(
+            string tableName,
+            string field,
+            object value)
+        {
+            string query =
+                "SELECT * FROM " + tableName +
+                " WHERE " + field + " = ";
 
+            if (value is string)
+                query += "'" + value + "'";
+            else
+                query += value;
+
+            return query;
+        }
+
+        // UPDATE
+        public string GenerateUpdateQuery(
+            string tableName,
+            string[] fields,
+            object[] values,
+            string whereField,
+            object whereValue)
+        {
+            string query =
+                "UPDATE " + tableName + " SET ";
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                query += fields[i] + " = ";
+
+                if (values[i] is string)
+                    query += "'" + values[i] + "'";
+                else
+                    query += values[i];
+
+                if (i < fields.Length - 1)
+                    query += ", ";
+            }
+
+            query += " WHERE " + whereField + " = ";
+
+            if (whereValue is string)
+                query += "'" + whereValue + "'";
+            else
+                query += whereValue;
+
+            return query;
+        }
+
+        // DELETE
+        public string GenerateDeleteQuery(
+            string tableName,
+            string field,
+            object value)
+        {
+            string query =
+                "DELETE FROM " + tableName +
+                " WHERE " + field + " = ";
+
+            if (value is string)
+                query += "'" + value + "'";
+            else
+                query += value;
+
+            return query;
+        }
     }
+
 }
